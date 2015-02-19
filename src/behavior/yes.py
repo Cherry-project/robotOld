a4892422123fff801d749abffc02579f1f8cab0d
Thomas <tom.lelep@free.fr>
2015-02-19 10:57:53 +0100

    modification de la façon de faire des comportement. A changer sans doute par la suite

--- src/behavior/yes.py
+++ src/behavior/yes.py
@@ -2,41 +2,44 @@ import numpy
 import json
 import time
 
-from pypot.vrep import from_vrep
-
+# from pypot.vrep import from_vrep
+from init import virtual_robot
 
 #TO DO : Changer en chemin absolu
 def init():
-	with open('../../utils/poppy_config.json') as f:			
-		poppy_config = json.load(f)
-	scene_path = '../../utils/poppy-standing2.ttt'
-	poppy = from_vrep(poppy_config,'127.0.0.1', 19997, scene_path)
-	# poppy.start_sync()
-	# for m in poppy.motors:
-	# 	m.compliant = False
-	return poppy
+    with open('../../utils/poppy_config.json') as f:            
+            poppy_config = json.load(f)
+    scene_path = '../../utils/poppy-standing2.ttt'
+    poppy = from_vrep(poppy_config,'127.0.0.1', 19997, scene_path)
+    # poppy.start_sync()
+    # for m in poppy.motors:
+    #   m.compliant = False
+    return poppy
 
 def run(robot):
-	center_position = 25
-	robot.head_y.goal_position = 90
-	t0 = time.time()
-	amp = 40
-	freq = 0.7
-	robot.head_y.goal_position = center_position
-	time.sleep(1)
+    center_position = 25
+    robot.head_y.goal_position = 90
+    t0 = time.time()
+    amp = 40
+    freq = 0.7
+    robot.head_y.goal_position = center_position
+    time.sleep(1)
 
-	while True:
-		t = time.time() - t0
-		if t > 7:
-			break
-		pos = amp * numpy.sin(2 * numpy.pi * freq * t) + center_position
-		robot.head_y.goal_position = pos 
-		time.sleep(0.02)
-	robot.head_y.goal_position = center_position
+    while True:
+        t = time.time() - t0
+        if t > 7:
+            break
+        pos = amp * numpy.sin(2 * numpy.pi * freq * t) + center_position
+        robot.head_y.goal_position = pos 
+        time.sleep(0.02)
+    robot.head_y.goal_position = center_position
 
 if __name__ ==  "__main__":
-	print "debut du test"
-	poppy = init()
-	print "fin de l'init"
-	run(poppy)
-	print "ok"
+    print "debut du test"
+    poppy = virtual_robot("coucou")
+    print poppy.test
+    poppy.init()
+    print poppy.test
+    print "fin de l'init"
+    run(poppy.poppy)
+    print "ok"
