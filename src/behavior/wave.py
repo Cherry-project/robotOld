fd61b24bbd716f5204703f7bfb16fb1b5dcdc6ed
Thomas <tom.lelep@free.fr>
2015-03-06 17:01:11 +0100

    correction typo wave.py (inversion de plusieur fréquences

--- src/behavior/wave.py
+++ src/behavior/wave.py
@@ -1,6 +1,7 @@
 import numpy
 import time
 
+
 import pypot.primitive
 from pypot.primitive.utils import Sinus
 
@@ -9,13 +10,14 @@ class WaveBehave(pypot.primitive.LoopPrimitive):
     def __init__(self, robot, freq):
         pypot.primitive.LoopPrimitive.__init__(self, robot, freq)
 
-        self.my_sinus = Sinus(self.robot, 50, [self.robot.l_elbow_y, ], amp=30, freq=freq, offset=-90)
+        self.my_sinus = Sinus(self.robot, 50, [self.robot.l_elbow_y, ], amp=30, freq=1, offset=-90)
 
     def setup(self):
         robot = self.robot
 
         for m in robot.l_arm:
             m.compliant = False
+
 
         robot.l_arm_z.goto_position(90, 1)
         robot.l_shoulder_x.goto_position(90,1)
@@ -25,6 +27,8 @@ class WaveBehave(pypot.primitive.LoopPrimitive):
         self.my_sinus.start()
 
     def teardown(self):
+        robot = self.robot
+
         self.my_sinus.stop()
 
         robot.l_arm_z.goto_position(0, 1)
