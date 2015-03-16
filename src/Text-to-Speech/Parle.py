5ae99a8483fb20a4ce12191bdd4e2eab4e2f78cb
unknown <lkhoudei@FR-33-01-12-060.corp.capgemini.com>
2015-03-16 14:27:42 +0100

    parle plusieurs phrases et s arret

--- src/Text-to-Speech/Parle.py
+++ src/Text-to-Speech/Parle.py
@@ -1,10 +1,41 @@
 import pyglet
 from gtts import gTTS
 
-tts = gTTS(text='Bonjour, je suis poppy le robot open source', lang='fr')
+# !!!!! Attention a n'utiliser que des caracteres ASCII, meme pour le speech du robot !!!!
+
+tts = gTTS(text='Bonjour,', lang='fr')
+tts.save("Speech.mp3")   # cree le mp3
+
+music = pyglet.media.load('Speech.mp3', streaming=False) 
+music.play()  #lit le mp3
+
+def exiter(dt):
+    pyglet.app.exit()
+pyglet.clock.schedule_once(exiter, music.duration)  #arrete la boucle a la fin du mp3
+
+pyglet.app.run()  #lance la boucle
+#______________NOM DE LA PERSONNE____________________________
+tts = gTTS(text='Maximilien', lang='fr')
+tts.save("Speech.mp3")  
+
+music = pyglet.media.load('Speech.mp3', streaming=False) 
+music.play() 
+
+def exiter(dt):
+    pyglet.app.exit()
+pyglet.clock.schedule_once(exiter, music.duration)
+
+pyglet.app.run()
+#__________________________________________
+tts = gTTS(text='Comment vas-tu ? Regarde comme je parle bien.', lang='fr')
 tts.save("Speech.mp3")
 
 music = pyglet.media.load('Speech.mp3', streaming=False) 
 music.play()
+
+def exiter(dt):
+    pyglet.app.exit()
+pyglet.clock.schedule_once(exiter, music.duration)
+
 pyglet.app.run()
 
