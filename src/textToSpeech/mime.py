4a3341feab32ce92ebabadbd1481be28057d9ca8
unknown <lkhoudei@FR-33-01-12-060.corp.capgemini.com>
2015-06-10 14:49:19 +0200

    plus jeux rmd

---
+++ src/textToSpeech/mime.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+
+import pypot.primitive
+import pypot.robot
+import random
+from pypot.primitive.move import MoveRecorder, Move, MovePlayer
+
+
+from speak import Speak
+
+class Mime(pypot.primitive.Primitive):
+
+    def __init__(self, robot):
+        pypot.primitive.Primitive.__init__(self, robot)
+
+        self._speak = Speak(robot)
+        self._move = "../src/moveRecorded/hello.move"
+        self._movem1 = "../src/moveRecorded/hello.move"
+        self._movem2 = "../src/moveRecorded/hello.move"
+        self._movem3 = "../src/moveRecorded/hello.move"
+        self._movem4 = "../src/moveRecorded/hello.move"
+        self._moveo = "../src/moveRecorded/hello.move"
+        self._moven = "../src/moveRecorded/hello.move"
+
+    def run(self):
+
+        poppy = self.robot
+
+        for m in poppy.motors:
+            m.compliant = False
+
+        num1 = random.randint(1,4)
+        
+
+        if num1 == 1 :
+            text1 = "singe"
+            mouvement = self._movem1
+        elif num1 == 2 :
+            text1 = "elephant"
+            mouvement = self._movem2
+        elif num1 == 3 :
+            text1 = "nager"
+            mouvement = self._movem3
+        elif num1 == 4: 
+            text1 = "manger"
+            mouvement = self._movem4
+        else:
+            text1 = "singe"
+            mouvement = self._movem1
+
+
+        text = "Devine ce que je mime."
+        
+        with open(mouvement) as f :
+
+            m = Move.load(f)
+    
+        move_player = MovePlayer(self.robot, m)
+        move_player.start()
+        
+        time.sleep(1.5)
+
+        self._speak.start(text)
+        print (text1)
+        print "appuyer sur o si le patient répond bien, sur n si il se trompe, puis sur entrée"
+
+        while True :
+            rep = raw_input()
+            #if appuie sur n
+            if rep == "n" :
+
+                with open(self._moven) as f :
+
+                    m = Move.load(f)
+    
+                move_player = MovePlayer(self.robot, m)
+                move_player.start()
+        
+                time.sleep(1.5)
+
+                self._speak.start("Non, ce n'est pas ça. Essaye encore!")
+
+            elif rep == "o" :
+                with open(self._moveo) as f :
+
+                    m = Move.load(f)
+    
+                move_player = MovePlayer(self.robot, m)
+                move_player.start()
+        
+                time.sleep(1.5)
+
+                self._speak.start("Bravo! Tu as vu comme je suis bon acteur?")
+                break
