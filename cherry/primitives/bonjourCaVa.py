5ce6ef6248b2899c8624292fc2902ed6f2c98a0b
tlelepvriercussol <tom.lelep@free.fr>
2016-02-26 16:16:31 +0100

    passage sous forme de bibliothèque. Toutes les primitives ne sont pas encore importées

---
+++ cherry/primitives/bonjourCaVa.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+import pypot.robot
+from pypot.primitive.move import MoveRecorder, Move, MovePlayer
+
+
+class BonjourCaVa(pypot.primitive.Primitive):
+    def __init__(self, robot):
+            pypot.primitive.Primitive.__init__(self, robot)
+            self._robot = robot
+        
+
+    def start(self):
+            
+            pypot.primitive.Primitive.start(self)
+
+    def run(self):
+     
+                move = '/home/poppy/cherry/resources/optional/move/bonjourCaVa.move'
+        
+                with open(move) as f:
+                    m = Move.load(f)
+                    
+                self._robot.compliant = False
+                
+                #
+                self._robot.speak.start("Bonjour, je me présente je m’appelle Cherry ! Comment t’appelles tu ?" )  
+                #
+                
+                move_player = MovePlayer(self._robot, m)
+                time.sleep(2)
+                move_player.start()
+                
